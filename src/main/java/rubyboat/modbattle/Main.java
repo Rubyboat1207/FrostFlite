5a5f874644c14658975fcbbda69c75679df70222
RudySoliz <rudy@rudysoliz.com>
2022-04-18 08:52:30 -0500

    2 block tall is hard

--- src/main/java/rubyboat/modbattle/Main.java
+++ src/main/java/rubyboat/modbattle/Main.java
@@ -4,13 +4,16 @@ import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
+import net.minecraft.block.Block;
 import net.minecraft.block.Material;
+import net.minecraft.block.TallPlantBlock;
 import net.minecraft.client.item.ModelPredicateProviderRegistry;
 import net.minecraft.item.*;
 import net.minecraft.sound.BlockSoundGroup;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 import rubyboat.modbattle.customBlocks.BroccoliPlantBlock;
+import rubyboat.modbattle.customBlocks.TallCropBlock;
 import rubyboat.modbattle.items.FarmingElytra;
 import rubyboat.modbattle.items.FarmingElytraEquipmentProvider;
 import rubyboat.modbattle.items.seedPackages.SeedBundle;
@@ -42,12 +45,17 @@ public class Main implements ModInitializer {
     public static final SeedBundle BEETROOT_BUNDLE = new SeedBundle(new FabricItemSettings().group(Main.RB_MODBATTLE_GROUP).maxCount(1), (BlockItem) Items.BEETROOT_SEEDS);
     public static final SeedBundle BAMBOO_BUNDLE = new SeedBundle(new FabricItemSettings().group(Main.RB_MODBATTLE_GROUP).maxCount(1), (BlockItem) Items.BAMBOO);
     public static final SeedBundle NETHER_WART_BUNDLE = new SeedBundle(new FabricItemSettings().group(Main.RB_MODBATTLE_GROUP).maxCount(1), (BlockItem) Items.NETHER_WART);
-    public static final BroccoliPlantBlock BROCCOLI_PLANT = new BroccoliPlantBlock(FabricBlockSettings.of(Material.PLANT).ticksRandomly().breakInstantly().sounds(BlockSoundGroup.CROP).nonOpaque().collidable(false));
-    public static final Item BROCCOLI_SEEDS = new BlockItem(BROCCOLI_PLANT, new FabricItemSettings().group(Main.RB_MODBATTLE_GROUP).maxCount(64));
     public static final SeedBundle BROCCOLI_BUNDLE = new SeedBundle(new FabricItemSettings().group(Main.RB_MODBATTLE_GROUP).maxCount(1), (BlockItem) Main.BROCCOLI_SEEDS);
+
     public static final FarmingElytra FARMING_ELYTRA = new FarmingElytra(new FabricItemSettings().group(Main.RB_MODBATTLE_GROUP).maxCount(1).equipmentSlot(FARMING_ELYTRA_EQUIPMENT_PROVIDER).maxDamage(400));
+
     public static final FoodComponent BROCCOLI_FOOD = new FoodComponent.Builder().hunger(6).saturationModifier(0.6f).build();
     public static final Item BROCCOLI = new Item(new FabricItemSettings().group(Main.RB_MODBATTLE_GROUP).maxCount(64).food(BROCCOLI_FOOD));
+    public static final BroccoliPlantBlock BROCCOLI_PLANT = new BroccoliPlantBlock(FabricBlockSettings.of(Material.PLANT).ticksRandomly().breakInstantly().sounds(BlockSoundGroup.CROP).nonOpaque().collidable(false));
+    public static final Item BROCCOLI_SEEDS = new BlockItem(BROCCOLI_PLANT, new FabricItemSettings().group(Main.RB_MODBATTLE_GROUP).maxCount(64));
+
+    public static final Block CORN_PLANT = new TallCropBlock(FabricBlockSettings.of(Material.PLANT).ticksRandomly().breakInstantly().sounds(BlockSoundGroup.CROP).nonOpaque().collidable(false));
+    public static final Item CORN_SEEDS = new TallBlockItem(CORN_PLANT, new FabricItemSettings().group(Main.RB_MODBATTLE_GROUP).maxCount(64));
 
     public static Item[] crops = new Item[] {
             Items.WHEAT_SEEDS, //done
@@ -70,7 +78,9 @@ public class Main implements ModInitializer {
         Registry.register(Registry.ITEM, new Identifier(MOD_ID, "broccoli_bundle"), BROCCOLI_BUNDLE);
         Registry.register(Registry.ITEM, new Identifier(MOD_ID, "broccoli_seeds"), BROCCOLI_SEEDS);
         Registry.register(Registry.ITEM, new Identifier(MOD_ID, "broccoli"), BROCCOLI);
+        Registry.register(Registry.ITEM, new Identifier(MOD_ID, "corn_seeds"), CORN_SEEDS);
         Registry.register(Registry.BLOCK, new Identifier(MOD_ID, "broccoli"), BROCCOLI_PLANT);
+        Registry.register(Registry.BLOCK, new Identifier(MOD_ID, "corn"), CORN_PLANT);
         ModelPredicateProviderRegistry.register(POTATO_BUNDLE, new Identifier(MOD_ID, "has_items"), (stack, world, entity, seed) -> stack.getOrCreateNbt().getInt(SeedBundle.KEY) > 0 ? 1 : 0);
         ModelPredicateProviderRegistry.register(CARROT_BUNDLE, new Identifier(MOD_ID, "has_items"), (stack, world, entity, seed) -> stack.getOrCreateNbt().getInt(SeedBundle.KEY) > 0 ? 1 : 0);
         ModelPredicateProviderRegistry.register(WHEAT_SEEDS_BUNDLE, new Identifier(MOD_ID, "has_items"), (stack, world, entity, seed) -> stack.getOrCreateNbt().getInt(SeedBundle.KEY) > 0 ? 1 : 0);
---
+++ src/main/java/rubyboat/modbattle/customBlocks/TallCropBlock.java
@@ -0,0 +1,79 @@
+package rubyboat.modbattle.customBlocks;
+
+import net.minecraft.block.*;
+import net.minecraft.block.enums.DoubleBlockHalf;
+import net.minecraft.entity.ItemEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.state.StateManager;
+import net.minecraft.state.property.BooleanProperty;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
+import net.minecraft.util.hit.BlockHitResult;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.MathHelper;
+import net.minecraft.world.BlockView;
+import net.minecraft.world.World;
+import rubyboat.modbattle.Main;
+
+import java.util.Random;
+
+public class TallCropBlock extends TallPlantBlock implements Fertilizable {
+    public static BooleanProperty isGrown = BooleanProperty.of("is_grown");
+
+
+    public TallCropBlock(Settings settings) {
+        super(settings);
+    }
+
+    @Override
+    protected boolean canPlantOnTop(BlockState floor, BlockView world, BlockPos pos) {
+        return floor.getBlock() == Blocks.FARMLAND;
+    }
+
+    @Override
+    public boolean isFertilizable(BlockView world, BlockPos pos, BlockState state, boolean isClient) {
+        return true;
+    }
+
+    @Override
+    public boolean canGrow(World world, Random random, BlockPos pos, BlockState state) {
+        return true;
+    }
+
+    @Override
+    public void grow(ServerWorld world, Random random, BlockPos pos, BlockState state) {
+        if(state.get(TallPlantBlock.HALF) == DoubleBlockHalf.UPPER) {
+            world.setBlockState(pos, state.with(isGrown, true));
+        }else {
+            world.setBlockState(new BlockPos(pos).add(0,1,0), state.with(isGrown, true));
+        }
+    }
+    @Override
+    public void appendProperties(StateManager.Builder<Block, BlockState> builder) {
+        builder.add(HALF);
+        builder.add(isGrown);
+    }
+
+    @Override
+    public ActionResult onUse(BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand, BlockHitResult hit) {
+        if(world.getBlockState(pos).get(isGrown)) {
+            world.setBlockState(new BlockPos(pos).add(0,world.getBlockState(pos).get(HALF) == DoubleBlockHalf.UPPER ? -1 : 1,0), state.with(isGrown, false));
+            world.spawnEntity(new ItemEntity(world, pos.getX(), pos.getY(), pos.getZ(), new ItemStack(Main.CORN_SEEDS)));
+            return ActionResult.SUCCESS;
+        }
+        return ActionResult.PASS;
+    }
+
+    @Override
+    public void randomTick(BlockState state, ServerWorld world, BlockPos pos, Random random) {
+        if(!state.get(isGrown)) {
+            if(random.nextInt(50) == 0) {
+                grow(world, random, pos, state);
+            }
+        }
+    }
+
+
+}
